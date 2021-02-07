bc15420bd8b6b2cac07bb0feedbb316086bb5924
Hruthik Sivakumar <67496262+hruthiksiva@users.noreply.github.com>
2021-02-07 12:22:18 +0530

    version 2

--- client_main.py
+++ client_main.py
@@ -1,4 +1,4 @@
-# -*- coding+9: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import socket
 from vidgear.gears import ScreenGear
@@ -6,53 +6,81 @@ from vidgear.gears import NetGear
 from pickle import dumps, loads
 import threading
 
-def chat_send():   
-    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    port = 12345                  
-    s.connect(('localhost', port, 0, 0))
+class Main():
+    ids = {}
     
-    print (s.recv(1024) )   
-    s.close()
-    
-def send_video():
-    stream = ScreenGear().start()
-    server = NetGear(port="54548", protocol="tcp")
-    print('Sharing Screen')
-    while True:
-        try:
-            frame = stream.read()
-            if frame is None:
-                break
-            server.send(frame)
-    
-        except KeyboardInterrupt:
-            break
+    def __init__(self, port = 12350):
+        self.sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        self.port = port
         
-    stream.stop()
-    server.close()
-    
-def recv_signal(sock):
-    sig = sock.recv(1024).decode()
-    
-    if sig == 'VID':
-        x = threading.Thread(target=send_video)
-        x.start()
         
-    elif sig == 'CHAT':
-        pass
+    def start(self, name = 'unknown'):
+        self.sock.connect(('localhost', self.port, 0, 0))
+        self.sock.send(name.encode())
+        self.recv_sig()
+        
+    def send_sig(self, sig):
+        self.sock.send(sig.encode())
+        
+    def recv_sig(self):
+        def inner():
+            while True:
+                sig = self.sock.recv(1024).decode()    
+                print(sig)
+                if sig == 'VID':
+                    self.send_video()
+                elif 'DENY' in sig :
+                    self.deny_ip(sig[5:])
+                elif 'ALLOW' in sig :
+                    self.deny_ip(sig[6:])
+                elif sig == 'CHAT':
+                    pass
+        self.ids['recv_sig'] = threading.Thread(target=inner)
+        self.ids['recv_sig'].start()
+            
+    def send_video(self, port = 54321):
+        def inner(port):
+            stream = ScreenGear().start()
+            server = NetGear(port=str(port), protocol="tcp")
+            while True:
+                try:
+                    frame = stream.read()
+                    if frame is None:
+                        break
+                    server.send(frame)            
+                except KeyboardInterrupt:
+                    break
+            stream.stop()
+            server.close()
+        self.ids['send_video'] = threading.Thread(target=inner, args=(port,))
+        self.ids['send_video'].start()
+        
+    def deny_ip(self, website = None):
+        with open(HOSTS_PATH, 'r+') as file: 
+            content = file.read()
+            if website in content: 
+                pass
+            else: 
+                file.write(f'{REDIRECT_IP} {website}\n')
     
+    def allow_ip(self, website = None):
+        with open(HOSTS_PATH, 'r+') as file: 
+            content=file.readlines() 
+            file.seek(0) 
+            for line in content: 
+                if not website in line: 
+                    file.write(line)
+            file.truncate()
+            
 if __name__ == '__main__':
-    
-    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    port = 12351
-    s.connect(('localhost', port, 0, 0))
+    REDIRECT_IP = '127.0.0.1'
+    #HOSTS_PATH = '/etc/hosts'
+    HOSTS_PATH = 'C:\Windows\System32\drivers\etc\hosts'
 
-    x = threading.Thread(target=recv_signal, args=(s,))
-    x.start()
+    main = Main()
     
+    temp1 = input('[ NAME ] > ')
+    main.start(temp1)
+
     while True:
-        x = input('')
-        if x == 'exit':
-            break
-        else:
-            pass
+        temp1 = input('[ LAN MONITOR CLI] > ')
--- server_main.py
+++ server_main.py
@@ -1,74 +1,118 @@
 # -*- coding: utf-8 -*-
 
-import socket, cv2, sys
+import socket, cv2
 from vidgear.gears import NetGear
 from pickle import dumps, loads
 import threading
 
-def chat():
-    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    port = 12349
-    s.bind(('', port))        
-    s.listen(5)   
-    print ("socket is listening")  
-    while True:  
-        c, addr = s.accept()      
-        print ('Got connection from', addr ) 
-        c.send('Thank you for connecting')
-        c.close()
-
-def recv_video():
-    client = NetGear(port="54548", protocol="tcp", receive_mode=True)
+class Main():
+    ids = {}
+    clients = {}
     
-    while True:
+    def __init__(self, port = 12350):
+        self.sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        self.port = port
         
-        frame = client.recv()
-        if frame is None:
-            break
+    def start(self, n_cli = 5):
+        self.sock.bind(('',self.port))
+        self.sock.listen(n_cli)
+        self.ids['recv_sig'] = threading.Thread(target=self.start_sig)
+        self.ids['recv_sig'].start()
         
-        cv2.imshow("Output Frame", frame)
+    def send_sig(self, c_sock, sig):
+        c_sock.send(sig.encode())
+        
+    def recv_sig(self):
+        sig = self.sock.recv(1024).decode()
+        
+    def start_sig(self):
+        while True :
+            c_sock, addr = self.sock.accept()
+            name = c_sock.recv(1024).decode()
+            self.clients[name] = [c_sock, addr]
     
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("q"):
-            break
+    def recv_video(self, port = 54321):
+        def inner(port):
+            client = NetGear(port = str(port), protocol="tcp", receive_mode=True)
+            while True:
+                frame = client.recv()
+                if frame is None:
+                    break
+                cv2.imshow("Output Frame", frame)
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord("q"):
+                    break
+            cv2.destroyAllWindows()
+            client.close()
+        self.ids['recv_video'] = threading.Thread(target=inner, args=(port,))
+        self.ids['recv_video'].start()
         
-    cv2.destroyAllWindows()
-    client.close()
+    def deny_ip(self, c_sock, website = None):
+        self.send_sig(c_sock, f'DENY {website}')
     
-def main():
-    global clients
-    
-    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    port = 12351
-    s.bind(('', port))
-    s.listen(5)
-    id = 0 
-    while True:  
-        c, addr = s.accept()
-        clients.append([id, c, addr])
-        id+=1
+    def allow_ip(self, c_sock,website = None):
+        self.send_sig(c_sock, f'ALLOW {website}')
         
-def send_signal(sock):
-    sock.send('VID'.encode())
+def printmsg(msgtype, msg, hdr = 'Undefined'):
+    if msgtype == 'log':
+        print(f'[ {hdr} ] > {msg}')
+    else:
+        print(msg)
     
 
 if __name__ == '__main__':
-    clients = []
+
+    help_str = """--------------->
+list - List the Clients Connected to the Lan Monitor
+peep - Peep into Clients Screen
+deny_ip - Block an ip address or website in clients computer
+allow_ip - Allow an ip address or website in clients computer
+exit - Exit the Program
+help - To list commands
+--------------->"""
+
+    main = Main()
+    main.start()
     
-    x = threading.Thread(target=main)
-    x.start()
-    
+    print(help_str)
     while True:
-        x = input('Enter you Choice =>')
-        if x == 'list':
-            for i in clients:
-                print(i[0])
-        elif x == 'peep':
-            temp2 = int(input('Enter you client ID =>'))
-            send_signal(clients[temp2][1]) 
-            threading.Thread(target=recv_video).start()
-        elif x == 'exit':
+        temp1 = input('[ LAN MONITOR ] > ')
+        if temp1 == 'list':
+            if main.clients == {}:
+                printmsg(None, 'No Clients Connected !')
+            else:
+                for clients in main.clients:
+                    printmsg(None, clients)
+                
+        elif temp1 == 'peep':
+            temp2 = input('[ Client Name ] > ')
+            if temp2 in main.clients.keys():
+                main.send_sig(main.clients[temp2][0],'VID') 
+                main.recv_video()
+            else:
+                printmsg('log', 'Invalid', f'No Client named {temp2} !')
+        
+        elif temp1 == 'deny_ip':
+            temp2 = input('[ Client Name ] > ')
+            temp3 = input('[ Website ] > ')
+            if temp2 in main.clients.keys():
+                main.deny_ip(main.clients[temp2][0], temp3)
+            else:
+                printmsg('log', 'Invalid', f'No Client named {temp2} !')
+                
+        elif temp1 == 'allow_ip':
+            temp2 = input('[ Client Name ] > ')
+            temp3 = input('[ Website ] > ')
+            if temp2 in main.clients.keys():
+                main.allow_ip(main.clients[temp2][0], temp3)
+            else:
+                printmsg('log', 'Invalid', f'No Client named {temp2} !')
+        
+        elif temp1 == 'help':
+            print(help_str)
+        
+        elif temp1 == 'exit':
             break
         else:
-            pass
+            printmsg('log', f'No command named {temp1} !, type help for more details.', 'Invalid')
     
